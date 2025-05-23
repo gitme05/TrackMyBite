1a5bb5c7a4a40d613a5e9fbd50b3cd5c96f7d122
Kerji Sabala <kerjisabala11@gmail.com>
2025-05-24 03:25:03 +0800

    my server route handlers, controllers like loginController.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const cookieParser = require('cookie-parser');
 
 const pagesRoutes = require('./back-end/routes/pages');
 
-const viewsPath = path.join(__dirname, 'front-end/views'); // ✅ DEFINE THIS
+const app = express();
+const PORT = 3000;
+const viewsPath = path.join(__dirname, 'front-end/views');
 
 app.use(cookieParser());
 app.use(express.json());
 
-// Static assets
+
 app.use('/front-end/styles', express.static(path.join(__dirname, 'front-end/styles')));
 app.use('/front-end/scripts', express.static(path.join(__dirname, 'front-end/scripts')));
 app.use('/front-end/assets', express.static(path.join(__dirname, 'front-end/assets')));
 
-// Routes
+
 app.use('/', pagesRoutes);
 
-// Start server
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
